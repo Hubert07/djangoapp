c439771d3e197fe99326cc1e1f11c86d16e0ea2b
Hubert07 <gajewski.hubert95@gmail.com>
2019-09-26 07:18:46 +0200

    dodanie stron jako widoki

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Pizza(models.Model):
+    LARGE = 'L'
+    MEDIUM = 'M'
+    SMALL = 'S'
+    ROZMIARY = (
+        (LARGE, 'Duża, 45cm'),
+        (MEDIUM, 'Średnia, 38cm'),
+        (SMALL, 'Mała, 25cm'),
+    )
+
+    nazwa = models.CharField(verbose_name='Pizza', max_length=30)
+    opis = models.TextField(blank=True, help_text='opis pizzy')
+    rozmiar = models.CharField(max_length=1, choices=ROZMIARY, default=MEDIUM)
+    cena =  models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Skladniki(models.Model):
+    pizza = models.ForeignKey()
--- pizza/views.py
+++ pizza/views.py
@@ -5,11 +5,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<center><h1>witaj w mojej pizzeri</h1></center>")
+    # return HttpResponse("<center><h1>witaj w mojej pizzeri</h1></center>")
+    return render(request, 'pizza/index.html')
 
 
 def komunikat(request):
-    return HttpResponse("<center><h1>Alert RCBG!</h1></center>")
+    return render(request, 'pizza/komunikat.html')
 
 
 def order(request):
