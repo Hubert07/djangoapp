e9d0b91fa7189fe659bb5bde12133fac9f199ff4
Hubert07 <gajewski.hubert95@gmail.com>
2019-10-03 08:45:11 +0200

    zmiany jakies

--- pizza/models.py
+++ pizza/models.py
@@ -11,10 +11,15 @@ class Pizza(models.Model):
     )
 
     nazwa = models.CharField(verbose_name='Pizza', max_length=30)
-    opis = models.TextField(blank=True, help_text='opis pizzy')
+    opis = models.TextField(blank=True, default='', help_text='opis pizzy')
     rozmiar = models.CharField(max_length=1, choices=ROZMIARY, default=MEDIUM)
-    cena =  models.DecimalField(max_digits=5, decimal_places=2)
+    cena =  models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    data = models.DateField('dodano', auto_now_add=True)
 
 
-# class Skladniki(models.Model):
-    # pizza = models.ForeignKey()
+class Skladniki(models.Model):
+    pizza = models.ManyToManyField(Pizza, related_name='skladniki')
+    nazwa = models.CharField(verbose_name='skladnik', max_length=30)
+    jarski = models.BooleanField(verbose_name='jarski?',
+                                 help_text='Zaznacz, jeżeli składnik jest odpowiedni dla wegetarian',
+                                 default=False)
